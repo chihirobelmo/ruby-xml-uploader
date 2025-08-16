f30d38eba2df3ac651eb8d1584c03f017d6ddcf7
Chihiro Suenaga <chihirobelmo@gmail.com>
2025-08-16 16:24:44 +0900

    fix validation and implement upload page view

--- app/models/xml_document.rb
+++ app/models/xml_document.rb
@@ -1,7 +1,16 @@
 # encoding: UTF-8
 class XmlDocument < ApplicationRecord
   has_one_attached :xml_file
-  
+
   validates :title, presence: true
-  validates :xml_file, presence: true, content_type: 'text/xml'
+  validates :xml_file, presence: true
+  validate :xml_file_type
+
+  private
+
+  def xml_file_type
+    if xml_file.attached? && xml_file.content_type != 'text/xml'
+      errors.add(:xml_file, 'はXMLファイルのみアップロードできます')
+    end
+  end
 end
