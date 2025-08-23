24fabbced1b8a1d6aaa24b787a137a40de2c2f79
Chihiro Suenaga <chihirobelmo@gmail.com>
2025-08-23 17:04:27 +0900

    add sign-up/log-in/log-out test

---
+++ test/integration/auth_flow_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class AuthFlowTest < ActionDispatch::IntegrationTest
+  test "sign up, login, logout flow" do
+    # Sign up
+    get "/signup"
+    assert_response :success
+    assert_select "form[action='/signup']"
+
+    assert_difference "User.count", 1 do
+      post "/signup", params: {
+        user: {
+          email: "flow@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_response :success
+
+  # Logout current session (integration tests can issue DELETE directly)
+  delete "/logout"
+    follow_redirect!
+    assert_response :success
+
+    # Login with remember me
+    get "/login"
+    assert_response :success
+    post "/login", params: {
+      email: "flow@example.com",
+      password: "password",
+      remember_me: "1"
+    }
+    follow_redirect!
+    assert_response :success
+  end
+end
