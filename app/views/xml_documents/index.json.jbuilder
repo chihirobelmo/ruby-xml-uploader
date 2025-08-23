ebfba1a1aeb62a762eb3dc8a38a0c1c11780ab8c
Chihiro Suenaga <chihirobelmo@gmail.com>
2025-08-23 13:57:46 +0900

    add GET command to get a list of xml files

---
+++ app/views/xml_documents/index.json.jbuilder
@@ -0,0 +1,14 @@
+json.array! @xml_documents do |doc|
+  json.extract! doc, :id, :title, :description, :created_at, :updated_at
+  if doc.xml_file.attached?
+    json.file do
+      json.filename doc.xml_file.filename.to_s
+      json.byte_size doc.xml_file.byte_size
+      json.content_type doc.xml_file.content_type
+      json.url rails_blob_url(doc.xml_file, only_path: true)
+      json.download_url rails_blob_path(doc.xml_file, disposition: "attachment", only_path: true)
+    end
+  else
+    json.file nil
+  end
+end
--- test/controllers/xml_documents_controller_test.rb
+++ test/controllers/xml_documents_controller_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class XmlDocumentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Rails.application.routes.url_helpers
+
+  setup do
+    @doc = XmlDocument.new(title: "Sample XML", description: "Fixture file")
+    # Attach a sample XML to the record before saving to pass validation
+    @doc.xml_file.attach(
+      io: File.open(Rails.root.join("test/fixtures/files/sample.xml")),
+      filename: "sample.xml",
+      content_type: "application/xml"
+    )
+    @doc.save!
+  end
+
+  test "should get index as JSON" do
+    get xml_documents_url(format: :json)
+    assert_response :success
+
+    body = JSON.parse(@response.body)
+    assert_kind_of Array, body
+    assert body.any?, "Expected at least one document in JSON response"
+    item = body.find { |h| h["id"] == @doc.id }
+    assert item, "Expected response to include created document"
+    assert_equal "Sample XML", item["title"]
+    assert item.key?("file"), "Expected file info key in response"
+    assert item["file"].key?("url"), "Expected file.url in response"
+  end
 end
